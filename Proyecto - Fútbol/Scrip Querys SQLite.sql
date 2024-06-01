74196ffed3dff4def3aae3894c8baf047322427c
David Guillén <167391150+Guillxn@users.noreply.github.com>
2024-06-01 00:02:17 -0400

    Ajustando script de SQL

--- Proyecto - Fútbol/Scrip Querys SQLite.sql
+++ Proyecto - Fútbol/Scrip Querys SQLite.sql
@@ -1,4 +1,5 @@
-/*1era Query*/
+/* 1era Query: Indicar los nombres y países de las 5 divisiones que tienen más
+partidos disputados en la base de datos */
 CREATE VIEW "Query_1" AS
 SELECT Cod_Divisiones, Nombre_Div, count (Cod_Division) AS Cantidad_Partidos, Pais FROM Divisiones
 		JOIN Partidos ON Cod_Divisiones=Cod_Division
@@ -6,10 +7,12 @@ SELECT Cod_Divisiones, Nombre_Div, count (Cod_Division) AS Cantidad_Partidos, Pa
 			ORDER BY Cantidad_Partidos DESC 
 			LIMIT 5;
 
-/*2da Query*/
+/* 2da Query: Indicar por temporada las estadísticas descriptivas de los goles
+anotados de local y de visitante por país */
 
 	
-/*3era Query*/
+/* 3era Query: Se desea saber temporada a temporada cuál fue el equipo de la
+liga Turca que más goles totales anotó */
 CREATE VIEW "q3" AS
 SELECT Cod_Division, Team_Local As Equipo, sum(Goles_Local) AS Total_Goles, Temporada FROM Partidos 
 	WHERE Cod_Division='T1'
@@ -26,6 +29,8 @@ SELECT Cod_Division, Equipo, Max(Total_Goles) AS Maximos_Goleadores, (Temporada)
 )
 	GROUP BY Temporada;
 
-/*4ta Query*/
+/* 4ta Query: Se quiere saber cuáles fueron los 10 equipos con más goles
+anotados como local indistintamente de la liga y la temporada */
 
-/*5ta Query*/
+/* 5ta Query: Se requiere observar todos los partidos con sus marcadores en el
+cual se enfrentaron el Real Madrid contra el Barcelona Futbol Club */
--- Proyecto - Fútbol/Scrip SQLite.sql
+++ Proyecto - Fútbol/Scrip SQLite.sql
@@ -1,7 +1,3 @@
-/*Estableciendo conexión entre tablas*/
-SELECT * FROM Divisiones 
-	JOIN Partidos on Cod_Divisiones=Cod_Division;
-
 /*Comandos para cambiar los valores*/
 
 UPDATE Partidos SET Ganador='V' WHERE Ganador='A';
@@ -42,3 +38,43 @@ CREATE VIEW laliga_2017_2018 AS
 SELECT Cod_Division, Team_Local, Goles_Local, Team_Visitante, Goles_Visitante, Ganador, Temporada 
 	FROM Partidos 
 	WHERE Cod_Division='SP1' AND Temporada IN ('2017-18', '2018-19');
+
+
+/* 1era Query: Indicar los nombres y países de las 5 divisiones que tienen más
+partidos disputados en la base de datos */
+CREATE VIEW "Query_1" AS
+SELECT Cod_Divisiones, Nombre_Div, count (Cod_Division) AS Cantidad_Partidos, Pais FROM Divisiones
+		JOIN Partidos ON Cod_Divisiones=Cod_Division
+			GROUP BY Cod_Division
+			ORDER BY Cantidad_Partidos DESC 
+			LIMIT 5;
+
+/* 2da Query: Indicar por temporada las estadísticas descriptivas de los goles
+anotados de local y de visitante por país */
+
+
+/* 3era Query: Se desea saber temporada a temporada cuál fue el equipo de la
+liga Turca que más goles totales anotó */
+CREATE VIEW "q3" AS
+SELECT Cod_Division, Team_Local As Equipo, sum(Goles_Local) AS Total_Goles, Temporada FROM Partidos 
+	WHERE Cod_Division='T1'
+		GROUP BY Team_Local, Temporada
+UNION 
+SELECT Cod_Division, Team_Visitante, sum(Goles_Visitante), Temporada FROM Partidos 
+	WHERE Cod_Division='T1'
+		GROUP BY Team_Local, Temporada;
+
+CREATE VIEW "Query_3" AS	
+SELECT Cod_Division, Equipo, Max(Total_Goles) AS Maximos_Goleadores, (Temporada) FROM 
+(SELECT Cod_Division, Equipo, sum(Total_Goles) AS Total_Goles, Temporada FROM q3
+	GROUP BY Equipo, Temporada
+	ORDER BY Temporada
+)
+	GROUP BY Temporada;
+
+/* 4ta Query: Se quiere saber cuáles fueron los 10 equipos con más goles
+anotados como local indistintamente de la liga y la temporada */
+
+
+/* 5ta Query: Se requiere observar todos los partidos con sus marcadores en el
+cual se enfrentaron el Real Madrid contra el Barcelona Futbol Club */
